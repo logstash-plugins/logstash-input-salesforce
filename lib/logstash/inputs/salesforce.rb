89da13e73005b55508c32098206c361f527273bb
Marc Sensenich <sensenichm91@gmail.com>
2019-06-19 08:01:12 -0400

    Support Scheduling for the Salesforce Input Plugin

--- lib/logstash/inputs/salesforce.rb
+++ lib/logstash/inputs/salesforce.rb
@@ -83,10 +83,17 @@ class LogStash::Inputs::Salesforce < LogStash::Inputs::Base
   config :sfdc_filters, :validate => :string, :default => ""
   # Setting this to true will convert SFDC's NamedFields__c to named_fields__c
   config :to_underscores, :validate => :boolean, :default => false
+  # Schedule of when to periodically run statement, in Cron format
+  # for example: "* * * * *" (execute query every minute, on the minute)
+  #
+  # There is no schedule by default. If no schedule is given, then the statement is run
+  # exactly once.
+  config :schedule, :validate => :string
 
   public
   def register
     require 'restforce'
+    require 'rufus/scheduler'
     obj_desc = client.describe(@sfdc_object_name)
     @sfdc_field_types = get_field_types(obj_desc)
     @sfdc_fields = get_all_fields if @sfdc_fields.empty?
@@ -94,6 +101,24 @@ class LogStash::Inputs::Salesforce < LogStash::Inputs::Base
 
   public
   def run(queue)
+    if @schedule
+      @scheduler = Rufus::Scheduler.new(:max_worker_thread => 1)
+      @scheduler.cron @schedule do
+        execute_query(queue)
+      end
+
+      @scheduler.join
+    else
+      execute_query(queue)
+    end
+  end # def run
+
+  def stop
+    @scheduler.shutdown(:wait) if @scheduler
+  end
+
+  private
+  def execute_query(queue)
     results = client.query(get_query())
     if results && results.first
       results.each do |result|
@@ -115,7 +140,7 @@ class LogStash::Inputs::Salesforce < LogStash::Inputs::Base
         queue << event
       end
     end
-  end # def run
+  end
 
   private
   def client
--- logstash-input-salesforce.gemspec
+++ logstash-input-salesforce.gemspec
@@ -21,9 +21,12 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency "logstash-core-plugin-api", ">= 1.60", "<= 2.99"
   s.add_runtime_dependency 'logstash-codec-plain'
   s.add_runtime_dependency 'restforce', '~> 3'
+  s.add_runtime_dependency 'rufus-scheduler'
+
   s.add_development_dependency 'logstash-devutils'
   s.add_development_dependency 'vcr'
   s.add_development_dependency 'webmock'
   s.add_development_dependency 'json'
   s.add_development_dependency 'public_suffix', '~> 1.4.6' # required due to ruby < 2.0
+  s.add_development_dependency 'timecop'
 end
